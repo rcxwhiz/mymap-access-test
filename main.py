d1495e9d7570c3f35e241ab47f1b3bb1de7c0936
josh-laptop <rcxwhiz@gmail.com>
2020-01-08 13:45:23 -0700

    Login almost works.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('test')
+import webbrowser
+
+# register the browser
+chrome_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+
+# webbrowser.get('chrome').open('lego.com')
+
+from selenium import webdriver
+browser = webdriver.Chrome()
+browser.get('https://mymap.byu.edu')
+username = browser.find_element_by_id('username')
+password = browser.find_element_by_id('password')
+username.send_keys('joshbedw')
+password.send_keys('')
+# password.submit()
+
+print('done')
