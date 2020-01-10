579fbe0399a9d1d195aae3c3adc8e4cdf7504bb2
josh-laptop <rcxwhiz@gmail.com>
2020-01-09 19:59:53 -0700

    The program can make a pickles directory and save a semester object to it.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import pickle
+import os
+import sys
+from pathlib import Path
 from classroom import semester_getter
 
-semester_getter.get('Fall 2019')
+# TODO make this so that it goes to the pickles folder within the current directory
+pickles_path = os.path.join(os.path.dirname(sys.argv[0]), 'pickles')
+if not os.path.exists(pickles_path):
+	os.makedirs(pickles_path)
+os.chdir(pickles_path)
+
+test_semester = semester_getter.get('Fall 2019')
 print('it worked')
+
+with open('test semester', 'wb') as test_file:
+	pickle.dump(test_semester)
+
+print('I was able to save the semester file!')
