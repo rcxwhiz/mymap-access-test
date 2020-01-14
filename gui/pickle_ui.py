dd5b2cb98452d9ff40cb3c36a50e19934db41086
josh-laptop <rcxwhiz@gmail.com>
2020-01-14 09:50:53 -0700

    Updated todo list, deleted old files, and made a pickles ui.

---
+++ gui/pickle_ui.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pickle_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(330, 269)
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(10, 10, 141, 16))
+        self.label.setObjectName("label")
+        self.tableView = QtWidgets.QTableView(Form)
+        self.tableView.setGeometry(QtCore.QRect(10, 40, 211, 171))
+        self.tableView.setObjectName("tableView")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(240, 50, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(240, 80, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 230, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton_3 = QtWidgets.QPushButton(Form)
+        self.pushButton_3.setGeometry(QtCore.QRect(140, 230, 75, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(250, 120, 61, 131))
+        self.label_2.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName("label_2")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "Cached Semesters"))
+        self.pushButton.setText(_translate("Form", "Delete"))
+        self.pushButton_2.setText(_translate("Form", "Refresh"))
+        self.pushButton_3.setText(_translate("Form", "Get Semester"))
+        self.label_2.setText(_translate("Form", "Note: Getting and refreshing semester may take about an hour."))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
