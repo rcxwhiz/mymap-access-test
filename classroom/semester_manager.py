f6ea0d5695473bd5579da010e27458eac4fbbe9e
josh-laptop <rcxwhiz@gmail.com>
2020-01-14 12:14:26 -0700

    Filtered list is now made (save days of the week).

--- classroom/semester_manager.py
+++ classroom/semester_manager.py
@@ -30,7 +30,7 @@ class SemesterManager(metaclass=SemesterManagerMeta):
 			self.semesters[file] = pickle.load(open(os.path.join(pickles_path, file), 'rb'))
 
 		self.selected_semester = None
-		self.filtered_courses = []
+		self.filtered_sections = []
 
 		self.dept_filter = ''
 		self.course_num_filter = ''
@@ -48,7 +48,7 @@ class SemesterManager(metaclass=SemesterManagerMeta):
 		                   'Thursday': False,
 		                   'Friday': False,
 		                   'Saturday': False}
-		self.credits_filter = [0.0, 0.0]
+		self.credits_filter = [0.0, 0.0]  # MIN - MAX
 		self.course_level_filter = {100: False,
 		                            200: False,
 		                            300: False,
@@ -75,9 +75,36 @@ class SemesterManager(metaclass=SemesterManagerMeta):
 
 	def select_semester(self, semester_year):
 		self.selected_semester = self.semesters[semester_year]
+		self.update_filtered_list()
 
 	def num_sections(self):
-		return len(self.filtered_courses)
+		return len(self.filtered_sections)
+
+	def update_filtered_list(self):
+
+		for course in self.selected_semester.courses:
+			if self.dept_filter not in course.dept:
+				pass
+			if self.course_num_filter not in str(course.num):
+				pass
+			if self.course_name_filter not in course.long_title:
+				pass
+			if True in self.course_level_filter.values():
+				if not self.course_level_filter[course.level]:
+					pass
+			for section in course.sections:
+				if self.instructor_filter not in section.instructor:
+					pass
+				if True in self.type_filter.values():
+					if not self.type_filter[section.type]:
+						pass
+				# TODO day filtering not enabled
+				if self.credits_filter[0] != 0 and self.credits_filter[1] != 0:
+					if not section.credits > self.credits_filter[0] or not section.credits < self.credits_filter[1]:
+						pass
+
+				# ALL TESTS HAVE BEEN PASSED
+				self.filtered_sections.append(section)
 
 
 # class SemesterManagerMeta(type):
